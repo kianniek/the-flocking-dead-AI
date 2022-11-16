a2b8d464475ae425ec6636a23af968029b3e436d
Kian <56929026+kianniek@users.noreply.github.com>
2022-11-16 21:28:16 +0100

    Makes sure the boids (seagulls) fligh away from the zombie seagulls

--- The Flocking Dead/Assets/Scripts/Behaviors/StandaloneBehaviors/EvadeBehavior.cs
+++ The Flocking Dead/Assets/Scripts/Behaviors/StandaloneBehaviors/EvadeBehavior.cs
@@ -7,6 +7,7 @@ public class EvadeBehavior : FlockBehavior
 {
     public override Vector2 CalculateMove(FlockAgent a, List<FlockAgent> context)
     {
+
         // if there is nothing in the context, return directly
         if (context.Count == 0)
             return Vector2.zero;
@@ -14,15 +15,29 @@ public class EvadeBehavior : FlockBehavior
         // find vector in average direction away from the context
         // aka the evade direction
         // but only if the other is a zombie
+        float closestDistance = float.MaxValue;
+        FlockAgent closestAgent = null;
+        float distance = 0;
         Vector2 direction = Vector2.zero;
+
         for (int i = 0; i < context.Count; i++)
         {
             // continue if other isn't zombie
             if (!context[i].isZombie)
                 continue;
 
+            // get the distance between the potential prey and myself
+            distance = (a.transform.position - context[i].transform.position).magnitude;
+
+            // if he's closer than the prev closest ...
+            if (distance < closestDistance)
+            {
+                // ... update the closest distance and agent 
+                closestDistance = distance;
+                closestAgent = context[i];
+            }
             // TODO: determine direction away from the zombie
-            // direction += ???;
+            direction += (Vector2)(a.transform.position - context[i].transform.position);
         }
 
         // average out the direction
